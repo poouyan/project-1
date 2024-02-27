09d70341b8063394968ee954b59b7c2964d7dd94
poouyan <159525676+poouyan@users.noreply.github.com>
2024-02-27 12:53:42 +0330

    Add files via upload

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 int main()
 {
-    printf("* * * *\n* * *\n* *\n*");
+    printf("112 111 117 121 97 110\t pouyan\n"); //print First Name
+    printf("104 101 105 100 97 114 98 111  122 111 114 103\t heidarbozorg");   //print Last Name
     return 0;
 }
