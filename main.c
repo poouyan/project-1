5e935202ac119a2234d9180cc482eb8c1ea8a7c9
poouyan <159525676+poouyan@users.noreply.github.com>
2024-02-27 21:47:05 +0330

    Add files via upload

--- main.c
+++ main.c
@@ -1,27 +1,41 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define MAX 10
 
 int main()
 {
-    char input[100];
-    printf("Enter a string in the format of \"1234,122342,yuer\":\n");
-    scanf("%s", input);
-    int index = 0;
-    int len = 0;
-    for (int i = 0; i < strlen(input); i++)
+    char users[MAX][20] = {"ali", "reza", "pouyan", "abod", "jasem"};
+    int count = 5;
+    char name[20];
+    printf("Enter your name:\n");
+    scanf("%s", name);
+    bool found = false;
+    for (int i = 0; i < count; i++)
     {
-        if (input[i] == ',')
+        if (strcmp(users[i], name) == 0)
         {
-            printf("index %d : %d len\n", index, len);
-            index++;
-            len = 0;
-
+            found = true;
+            break;
+        }
+    }
+    if (found)
+    {
+        printf("Welcome, %s. You have successfully logged in.\n", name); // print a welcome message
+    }
+    else
+    {
+        if (count < MAX)
+        {
+            strcpy(users[count], name);
+            count++;
+            printf("Hello, %s. You are a new user. Your name has been added to the array.\n", name); // print a greeting message
         }
         else
         {
-            len++;
+            printf("Sorry, %s. The array is full. You cannot be added as a new user.\n", name); // print an error message
         }
     }
-    printf("index %d : %d len\n", index, len);
     return 0;
 }
-
